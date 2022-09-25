9bbcad750a18ce483ae03d3b2e9e755124becbc5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-25 11:38:09 -0500

    feat: add dropdown list to select the available currencies

--- 03-currency_converter.py
+++ 03-currency_converter.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from PyQt6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
-                             QVBoxLayout, QWidget)
+from PyQt6.QtWidgets import (QApplication, QComboBox, QLabel, QLineEdit,
+                             QPushButton, QVBoxLayout, QWidget)
+
+CURRENCIES = ["USD", "MXN", "EUR"]
 
 
 def get_currency(in_currency, out_currency):
@@ -16,9 +18,12 @@ def get_currency(in_currency, out_currency):
 
 def show_currency():
     input_text = float(text.text())
-    rate = get_currency("USD", "MXN")
+    in_currency = in_combo.currentText()
+    target_currency = target_combo.currentText()
+    rate = get_currency(in_currency, target_currency)
     output = round(input_text * rate, 2)
-    output_label.setText(str(output))
+    message = f"{input_text} {in_currency} = {output} {target_currency}"
+    output_label.setText(message)
 
 
 app = QApplication([])
@@ -26,6 +31,14 @@ window = QWidget()
 window.setWindowTitle('Currency Converter')
 
 layout = QVBoxLayout()
+
+in_combo = QComboBox()
+in_combo.addItems(CURRENCIES)
+layout.addWidget(in_combo)
+
+target_combo = QComboBox()
+target_combo.addItems(CURRENCIES)
+layout.addWidget(target_combo)
 
 text = QLineEdit()
 layout.addWidget(text)
