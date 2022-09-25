dec44ac01e88e85c5aaa79e0b3016da5ded5c663
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-25 09:21:31 -0500

    feat: add post request

--- 01-flask-web-app.py
+++ 01-flask-web-app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def home_post():
+    dim1 = request.form['first_dim']
+    dim2 = request.form['second_dim']
+    dim3 = request.form['third_dim']
+    volume = float(dim1)*float(dim2)*float(dim3)
+    return render_template('index.html', output=volume, dim_1=dim1, dim_2=dim2, dim_3=dim3)
 
 
 app.run(host='localhost', port=5000, debug=True)
